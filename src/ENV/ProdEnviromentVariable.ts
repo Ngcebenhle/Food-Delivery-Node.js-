190749eed661b151468510d0a693b017310949be
unknown <ngcebenhleshabangu@gmail.com>
2024-07-11 18:37:17 +0200

    Clothe Again

--- src/ENV/ProdEnviromentVariable.ts
+++
@@ -1,5 +0,0 @@
-export const prodENV = {
-    db_uri: 'mongodb+srv://zaiotestapp:7fYLfUvsc5Ic4qLt@zaiotestapp.nityybv.mongodb.net/?retryWrites=true&w=majority&appName=ZaioTestApp',
-
-    jwt_secret_key: 'me'
-}
